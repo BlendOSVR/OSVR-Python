3b30c47fd29b33f4db00ec1dc32a3012708039ea
awang94 <adam.wang94@gmail.com>
2015-12-07 06:40:11 -0500

    removed extra example files

--- DisplayParameterPy.py
+++
@@ -1,9 +0,0 @@
-import osvr.ClientKit
-
-ctx = osvr.ClientKit.ClientContext("com.osvr.exampleclients.DisplayParameter")
-path = "/display"
-displayDescription = ctx.getStringParameter(path)
-
-print("Got value of %s: \n%s" % (path, displayDescription))
-ctx.shutdown()
-print("Library shut down, exiting.")
